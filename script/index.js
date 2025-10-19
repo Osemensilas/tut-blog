d852174ed60d5bf1cdde585a786751e4aaaeec70
unknown <osemensilas@gmail.com>
2025-10-19 20:18:27 +0100

    New Commit

---
+++ script/index.js
@@ -0,0 +1,80 @@
+/**
+ * 
+ * Variables in JavaScript
+ * 
+ * String
+ * 
+ * Boolean
+ * 
+ * Interger
+ * 
+ * null
+ * 
+ * const, var, let
+**/
+
+// const firstName = "Osemen";
+
+// const myNumber = "4";
+
+// const firstNumber = 5;
+// const secondNumber = 8;
+
+// const myFloat = 16.7;
+
+// const isHuman = true;
+
+// const myColor = "blue";
+
+// const beverage = ['milk', 'sugar', 'bread', 'butter'];
+
+// const rain = true;
+
+// if (rain){
+//     console.log("Use Umbrella")
+// }else{
+//     console.log("Do not use umbrella");
+// }
+
+// const myAge = 20;
+
+// if (myAge < 18 || myAge > 45){
+//     console.log("You do not have access. Bye Bye");
+// }else{
+//     console.log("Welcome.");
+// }
+
+// const firstToss = "Tail";
+// const secondToss = "Head";
+
+// if (firstToss === "Head" && secondToss === "Tail"){
+//     console.log("Game over");
+// }else if(firstToss === "Tail" && secondToss === "Head"){
+//     console.log("Game over");
+// }else{
+//     console.log("You win");
+// }
+
+//Loop
+
+// const weekDays = ['mon', 'tue', 'wed', 'thurs', 'fri', 'sat', 'sun'];
+
+//console.log(weekDays);
+//console.log(weekDays[5]);
+
+// For loop
+// for (let i = 0; i < weekDays.length; i++){
+//     let weekDay = weekDays[i];
+// }
+
+// weekDays.forEach((weekDay, index) => {
+//     console.log(weekDay, index);
+// })
+
+const btn = document.querySelector('.hero-cta');
+//const btn = document.getElementsByClassName('hero-cta');
+//const btn = document.getElementsByTagName('button');
+
+btn.addEventListener('click', function(){
+    
+})
